6e274161dd154b2a155daf49df6ffb0065062809
Tom Wayson <tom@tomwayson.com>
2016-05-07 22:11:37 -0700

    when moving rows, ghost whole row, not just handle
    using .setDragImage(), which is not supported on IE11

--- app/components/layout-row-editor/component.js
+++ app/components/layout-row-editor/component.js
@@ -8,6 +8,18 @@ export default Ember.Component.extend({
   classNameBindings: ['active'],
   active: false,
 
+  // set the drag image
+  _setDragImage(e) {
+    if (!e.dataTransfer || !e.dataTransfer.setDragImage) {
+      return;
+    }
+    const $row = this.$();
+    const $handle = this.$(e.target);
+    // b/c we're dragging from upper right corner,
+    // want to shift to the left by width of the element
+    e.dataTransfer.setDragImage($row[0], $row.outerWidth() - ($handle.outerWidth() / 2), $handle.outerHeight() / 2);
+  },
+
   dragEnter(e) {
     this.set('active', true);
     this.sendAction('onDragEnter', e, this.model);
@@ -32,6 +44,7 @@ export default Ember.Component.extend({
   actions: {
     onRowDragStart (e) {
       e.dataTransfer.setData('text/plain', 'moveRow');
+      this._setDragImage(e);
       this.sendAction('onStartMove', e, this.model);
     }
   }
